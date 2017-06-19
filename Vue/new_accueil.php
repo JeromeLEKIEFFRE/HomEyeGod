2f40c68c8048c7200c99b6373f70f29ba57192b0
Sylphorn <eliott.vanacker@isep.fr>
2017-06-19 09:56:43 +0200

    merge accueil et inscription

---
+++ Vue/new_accueil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style_new_accueil.css" />
+    <title>GerHome</title>
+</head>
+
+<body>
+<header>
+    <h1><a href="home_page.html"><img src="Images/logo-accueilDiv2.png"  alt="logo GerHome" /></a></h1>
+</header>
+<div class="titre_page">
+    <h1>GerHome</h1>
+</div>
+
+
+<section class="connexion_accueil" id="cadre_connexion">
+
+    <form action="../Controleur/Formulaire.php" method="post">
+        <label for="Nom d'utilisateur"> Nom d'utilisateur:</label><input type="text" name="Nom_utilisateur" /><br/>
+        <label for="Mot de passe"> Mot de passe:</label><input type="password" name="Mot_de_passe" /><br/>
+        <label for="valider"></label><input type="submit" name="valider" />
+        <p> pas encore de compte?<a href="inscription.html"> inscrivez vous!</a></p>
+
+    </form>
+</section>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
+
+<?php /**
+ * Created by PhpStorm.
+ * User: eliot
+ * Date: 02/06/2017
+ * Time: 10:38
+ */
