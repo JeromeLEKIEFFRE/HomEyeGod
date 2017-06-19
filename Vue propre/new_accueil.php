85ffaf175938c0b9631231e22b2e7518c6be64ff
Sylphorn <eliott.vanacker@isep.fr>
2017-06-19 10:03:18 +0200

    update des images et réorganisation

--- Vue propre/new_accueil.php
+++ Vue propre/new_accueil.php
@@ -8,7 +8,7 @@
 
 <body>
 <header>
-    <h1><a href="home_page.html"><img src="Images/logo-accueilDiv2.png"  alt="logo GerHome" /></a></h1>
+    <h1><a href="../Vue/home_page.html"><img src="../Vue/Images/logo-accueilDiv2.png" alt="logo GerHome" /></a></h1>
 </header>
 <div class="titre_page">
     <h1>GerHome</h1>
@@ -21,7 +21,7 @@
         <label for="Nom d'utilisateur"> Nom d'utilisateur:</label><input type="text" name="Nom_utilisateur" /><br/>
         <label for="Mot de passe"> Mot de passe:</label><input type="password" name="Mot_de_passe" /><br/>
         <label for="valider"></label><input type="submit" name="valider" />
-        <p> pas encore de compte?<a href="inscription.html"> inscrivez vous!</a></p>
+        <p> pas encore de compte?<a href="../Vue/inscription.html"> inscrivez vous!</a></p>
 
     </form>
 </section>
