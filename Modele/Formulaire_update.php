cb77cda19756f48d19fe16524d06ea73fb80fb94
CorentinBetz <corentinbetz@gmail.com>
2017-06-21 15:35:08 +0200

    page gestion compte

---
+++ Modele/Formulaire_update.php
@@ -0,0 +1,13 @@
+<?php
+include ("connexion_db.php");
+if ($_POST["Nom_utilisateur"]!=Null){$db->exec('UPDATE utilisateurs  SET NomUtilisateur= "'.$_POST ["Nom_utilisateur"].'" WHERE idUtilisateur = 1');}
+if ($_POST ["Mot_de_passe"]!=Null && $_POST ["Mot_de_passe"]==$_POST ["confirmer"]){$db->exec('UPDATE utilisateurs  SET Mdp= "'.$_POST ["Mot_de_passe"].'" WHERE idUtilisateur = 1');}
+if ($_POST["email"]!=Null){$db->exec('UPDATE utilisateurs  SET Mail = "'.$_POST ["email"].'" WHERE idUtilisateur = 1');}
+if ($_POST["mobile"]!=Null){$db->exec('UPDATE utilisateurs  SET Mobile = "'.$_POST ["mobile"].'" WHERE idUtilisateur = 1');}
+if ($_POST["fixe"]!=Null){$db->exec('UPDATE utilisateurs  SET numero = "'.$_POST ["fixe"].'" WHERE idUtilisateur = 1');}
+if ($_POST["code_postal"]!=Null){$db->exec('UPDATE maisons  SET code_postal = "'.$_POST ["code_postal"].'" WHERE idUtilisateur = 1');}
+if ($_POST["Ville"]!=Null){$db->exec('UPDATE maisons  SET Ville = "'.$_POST ["Ville"].'" WHERE idUtilisateur = 1');}
+if ($_POST["numero_voie"]!=Null){$db->exec('UPDATE maisons  SET numero_voie = "'.$_POST ["numero_voie"].'" WHERE idUtilisateur = 1');}
+if ($_POST["TypeValue"]!=Null){$db->exec('UPDATE maisons  SET TypeVoie = "'.$_POST ["TypeValue"].'" WHERE idUtilisateur = 1');}
+if ($_POST["Voie"]!=Null){$db->exec('UPDATE maisons  SET Voie = "'.$_POST ["Voie"].'" WHERE idUtilisateur = 1');}
+?>
---
+++ Vue/gestioncompte.php
@@ -0,0 +1,107 @@
+<head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Créer un compte</title>
+</head>
+
+<?php
+$dbname = 'homeyegod';
+$host='localhost';
+$user='root';
+$password='root';
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$password");
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$infoU1 = $bdd->query('SELECT * FROM utilisateurs WHERE idUtilisateur = 1');
+$infoU=$infoU1 -> fetch();
+$infoM1 = $bdd->query('SELECT * FROM maisons WHERE idUtilisateur = 1');
+$infoM=$infoM1 -> fetch();
+$p=$infoM['TypeVoie'];
+$infoV1 = $bdd->query('SELECT TypeName FROM typevoie WHERE TypeValue = "'.$p.'"');
+$infoV=$infoV1 -> fetch();
+?>
+    <body>
+        <header>
+            <h1><a href="home_page.html"><img src="logo-accueil.png" height="15%" width="15%" alt="logo HomEyeGod" /></a></h1>
+        </header>
+        <nav>
+            <ul id="navigation">
+                <li><a href="home_page.html" alt="logo Home">Accueil <!--<img src="logo_accueil.png" alt="retour vers l'accueil"/></a></li>-->   <!-- ici mettre le logo accueil -->
+                <li><a href="mon_compte.html">Mon Compte</a></li>
+                <li><a href="ma_maison.html">Ma maison</a></li>
+                <li><a href="boutique.html">Boutique</a></li>
+                <li><a href="qui_sommes_nous.html">Qui sommes nous?</a></li>
+                <li><a href="aide_contact.html">Aide/Contact</a></li>
+            </ul>
+        </nav>
+
+        <section>
+        	<h1>Données personnelles</h1>
+            <div class="gestion_c">
+            <aside>
+        		<p>Sexe <?php ?><br/>
+        		Nom<br/>
+        		Prénom  <br/>
+        		Nom d'utilisateur<br/>
+        		Code postal<br/>
+        		Ville<br/>
+        		adresse<br/>
+        		email<br/>
+        		Numéro de téléphone<br/>
+                Numéro de téléphone Fixe</p>
+            </aside>
+        	<aside>
+                <p> homme<br/>
+                    <?php echo $infoU['Nom']?><br/>
+                    <?php echo $infoU['Prenom']?><br/>
+                    <?php echo $infoU['NomUtilisateur']?><br/>
+                    <?php echo $infoM['numero_voie'],' ',$infoV['TypeName'],' ',$infoM['Voie'] ?><br/>
+                    <?php echo $infoM['code_postal']?><br/>
+                    <?php echo $infoM['Ville']?><br/>
+                    <?php echo $infoU['Mail']?><br/>
+                    <?php echo $infoU['Mobile']?><br/>
+                    <?php echo $infoU['Numero']?><br/>
+                </p>
+        	</aside>
+            </div>
+            <?php
+            $Type1 = $bdd->query('SELECT * FROM typevoie');
+            $Type = $Type1 -> fetchAll();
+            ?>
+        	<h1>Changer données personnelles</h1>
+                <form action="../Modele/Formulaire_update.php" method="post">
+        		<label for="Nom d'utilisateur"> Nom d'utilisateur:</label><input type="text" name="Nom_utilisateur" /><br/>
+				<label for="Mot de passe"> Mot de passe:</label><input type="password" name="Mot_de_passe" /><br/>
+                <label for="Confirmer le mot de passe"> Confimer le mot de passe:</label><input type="password"  name="confirmer"/> <br/>
+                <label for="Numero voie"> Adresse:</label><input type="text" name="numero_voie"/>
+                    <select name="TypeValue">
+                        <?php foreach ($Type as $ligne){ ?>
+                            <option value=<?=$ligne["TypeValue"]?>>
+                                <?= $ligne['TypeName']?></option>
+                        <?php } ?>
+                    </select>
+                <label for="Voie"></label><input type="text" name="Voie"/><br/>
+                <label for="Code postal"> Code postal:</label><input type="text" name="code_postal"/><br/>
+                <label for="Vile"> Ville:</label><input type="text" name="Ville"/><br/>
+                <label for="email"> email:</label><input type="email" name="email"/><br/>
+                <label for="Numéro de téléphone"> Numéro de téléphone:</label><input type="tel" name="mobile"/><br/>
+                <label for="Numéro de téléphone fixe"> Numéro de téléphone fixe:</label><input type="tel" name="fixe"/><br/>
+
+            <h1>Saisie du mot de passe actuel pour confirmé</h1>
+				<label for="valider"></label><input type="submit" name="valider" />
+                </form>
+            <aside>
+            </aside>
+            <article>                
+            </article>
+        </section>
+        
+        <footer>
+            
+        </footer>
+        
+    </body>
