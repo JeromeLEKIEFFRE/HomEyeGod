40b67f76581bae5a9388031ee26420c4da18c33e
JeromeLEKIEFFRE <jerome.lekieffre@isep.fr>
2017-06-26 17:52:56 +0200

    Merge branch 'Infocapt'
    # Conflicts:
    #	Modele/connexion_db.php
    #	Vue/styleGestionMaison.css

--- Controleur/Formulaire_connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ("../Modele/Verification.php");
-include("../Modele/connexion_db_1.php");
-if (test_connect($db,$_POST['Nom_utilisateur'],$_POST['Mot_de_passe'])){
-    include ("session.php");
-    setcookie($_POST['Nom_utilisateur'],get_name ($db,$username)." ".get_first_name ($db,$username));
-    header('Location: ../'); // redirection vers la page gestion_compte
-}
