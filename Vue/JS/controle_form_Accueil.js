93cece3bb58527893cd23bb59005f876006946a7
Jimmy <wangjimmy25@gmail.com>
2017-06-26 22:21:15 +0200

    Ajout du JS pour Inscription

--- Vue/JS/controle_form_Accueil.js
+++ Vue/JS/controle_form_Accueil.js
@@ -1,18 +1,86 @@
+function Afficher_erreur(){
+    var a = document.forms["form_inscription"]["nom"].value;
+    var b = document.forms["form_inscription"]["prenom"].value;
+    var c = document.forms["form_inscription"]["sexe"].value;
+    var d = document.forms["form_inscription"]["username"].value;
+    var e = document.forms["form_inscription"]["mdp"].value;
+    var f = document.forms["form_inscription"]["confirm_mdp"].value;
+    var g = document.forms["form_inscription"]["numero_voie"].value;
+    var h = document.forms["form_inscription"]["voie"].value;
+    var i = document.forms["form_inscription"]["code_postal"].value;
+    var j = document.forms["form_inscription"]["ville"].value;
+    var k = document.forms["form_inscription"]["email"].value;
+    var l = document.forms["form_inscription"]["numero_tel"].value;
+    var m = document.forms["form_inscription"]["numero_tel_fixe"].value;
+    var erreur_nom="";
+    var erreur_prenom="";
+    var erreur_sexe="";
+    var erreur_username="";
+    var erreur_mdp="";
+    var erreur_confirm_mdp="";
+    var erreur_noconfirm="";
+    var erreur_numero_voie="";
+    var erreur_voie="";
+    var erreur_code_postal="";
+    var erreur_ville="";
+    var erreur_email="";
+    var erreur_numero_tel="";
+    var erreur_numero_tel_fixe="";
 
-var you_shall_not_pass = function (event) {
-
-    if(document.getElementsByName("nom")[0].value == ""){  // Cas Nom pas précisé
-
-        alert("Vous n'avez pas précisé de nom");
-        event.preventDefault();
-        return;
+    if (a == ""){
+        erreur_nom="Veuillez entrer un nom";
     }
-    if(!document.querySelector('input[name="sexe"]:checked')){ // Si !Objet -> false, !Null -> true
-        alert("Vous n'avez pas de sexe");
-        event.preventDefault(); // empeche de lancer le submit
-        return;
+    if (b == ""){
+        erreur_prenom="Veuillez entrer un prénom";
     }
-    return;
-};
-document.getElementById("form_inscription").addEventListener("submit", you_shall_not_pass, false); // ON ajoute un écouteur lorsqu'on clique sur envoyer
-console.log(document.getElementsByName("nom")[0].value == "");
+    if (c ==""){
+        erreur_sexe="Veuillez sélectionner un sexe";
+    }
+    if (d ==""){
+        erreur_username="Veuillez entrer un nom d'utilisateur";
+    }
+    if (e ==""){
+        erreur_mdp="Veuillez entrer un mot de passe";
+    }
+
+    else {
+        if (f == "") {
+            erreur_confirm_mdp = "Veuillez confirmer votre mot de passe";
+        }
+        else {
+            if (e != f) {
+                erreur_noconfirm = "Vous n'avez pas rentré le même mot de passe";
+            }
+        }
+    }
+
+    if (g ==""){
+        erreur_numero_voie="Veuillez entrer un numéro de voie";
+    }
+    if (h ==""){
+        erreur_voie="Veuillez entrer une voie";
+    }
+    if (i ==""){
+        erreur_code_postal="Veuillez entrer un code postal";
+    }
+    if (j ==""){
+        erreur_ville="Veuillez entrer une ville";
+    }
+    if (k ==""){
+        erreur_email="Veuillez entrer une adresse mail";
+    }
+    if (l ==""){
+        erreur_numero_tel="Veuillez entrer un numéro de téléphone portable";
+    }
+    if (m ==""){
+        erreur_numero_tel_fixe="Veuillez entrer un numéro de téléphone fixe";
+    }
+
+    if (a==""||b==""||c==""||d==""||e==""||f==""||e!=f||g==""||h==""||i==""||j==""||k==""||l==""||m==""){
+        alert(erreur_nom + '\n' + erreur_prenom + '\n' + erreur_sexe + '\n' + erreur_username + '\n' + erreur_mdp
+            + '\n' + erreur_confirm_mdp + '\n' + erreur_noconfirm + '\n' + erreur_numero_voie + '\n' + erreur_voie + '\n' + erreur_code_postal
+            + '\n' + erreur_ville + '\n' + erreur_email + '\n' + erreur_numero_tel + '\n' + erreur_numero_tel_fixe);
+        return false;
+
+    }
+}
