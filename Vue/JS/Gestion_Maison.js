78356bd7056ae6c1a0d84706582c4bd8a2081f56
JeromeLEKIEFFRE <jerome.lekieffre@isep.fr>
2017-06-26 19:11:22 +0200

    Add files via upload

---
+++ Vue/JS/Gestion_Maison.js
@@ -0,0 +1,47 @@
+/**
+ * Created by jlWin on 26/06/2017.
+ */
+function cachershowgeneral(){
+    var x = document.getElementById('showgeneral');
+    var y = document.getElementById('showdatas');
+    x.style.display="flex";
+    y.style.display="flex";
+}
+
+function blabla(str) {
+    if(str==""){
+        return;
+    }
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        if (window.ActiveXObject)
+            try {
+                xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                    return NULL;
+                }
+            }
+    }
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("showdatas").innerHTML = xmlhttp.responseText;
+            document.getElementById("idpiece").value = str;
+            console.log(xmlhttp.responseText);
+
+            document.getElementById('showdatas').style.display='flex';
+
+        }
+    }
+    xmlhttp.open("GET","Gestion_Maison.php?pid="+str,true);
+    xmlhttp.send();
+}
+
+function affichersg(){
+    document.getElementById('showdatas').style.display='flex';
+    document.getElementById('showdatas')
+}
