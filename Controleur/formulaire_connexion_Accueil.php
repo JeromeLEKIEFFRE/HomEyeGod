6f8ad94a6db6485883a14108d0cd8c17c4aebb7e
CorentinBetz <corentinbetz@gmail.com>
2017-06-26 18:50:25 +0200

    ajouts session

--- Controleur/formulaire_connexion_Accueil.php
+++ Controleur/formulaire_connexion_Accueil.php
@@ -7,7 +7,10 @@ if (connexion_check_password($db, $_POST["Nom_utilisateur"])["Mdp"]==$_POST["Mot
     $_SESSION['idUtilisateur']=connexion_check_password($db,$_POST["Nom_utilisateur"])["idUtilisateur"];
     $_SESSION['Roles']=connexion_check_password($db,$_POST["Nom_utilisateur"])["Roles"];
      if($_SESSION['Roles']==1){
-         header('');
+     }
+     if($_SESSION['Roles']==2){
+         header('Location: ../Vue/Pages/page_admin.php');
+         exist();
      }
 }
 else{
