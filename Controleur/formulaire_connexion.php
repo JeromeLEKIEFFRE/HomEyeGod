b65cd65a6c4c9b55523d51240b8fd9512f8abeab
Sylphorn <eliott.vanacker@isep.fr>
2017-06-26 15:02:33 +0200

    update

--- Controleur/formulaire_connexion.php
+++ Controleur/formulaire_connexion.php
@@ -1,4 +1,8 @@
 <?php
 include ("../Modele/connexion_db.php");
 
-if ($_POST["username"])
+if ($_POST["username"] != NULL)
+    $username=$_POST["username"];
+
+if ($_POST["password"] != NULL)
+    $password=$_POST["password"]
--- Modele/request_session.php
+++ Modele/request_session.php
@@ -1,1 +1,18 @@
 <?php
+include ("../Modele/connexion_db.php");
+
+function connexion_check_username($db, $username)
+{
+    $sql='SELECT idUtilisateur FROM utilisateurs WHERE NomUtilisateur="'.$username.'"';
+    $rep = $db->query($sql);
+    $repf=$rep->fetch();
+    return $repf;
+}
+function connexion_check_password($db, $password, $idUtilisateur)
+{
+    $sql='SELECT idUtilisateur FROM utilisateurs WHERE idUtiliateur="'.$idUtilisateur.'"';
+    $rep = $db->query($sql);
+    $repf=$rep->fetch();
+    return $repf;
+}
+function password_verify{}
