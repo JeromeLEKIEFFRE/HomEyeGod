86fa3a99641f5bbc41675fb8cadef9704f360b9d
JeromeLEKIEFFRE <jerome.lekieffre@isep.fr>
2017-06-27 13:20:45 +0200

    Merge branch 'Infocapt'
    # Conflicts:
    #	Modele/connexion_db.php
    #	Vue/styleGestionMaison.css

--- Controleur/Controleur_gestion_maison.php
+++ Controleur/Controleur_gestion_maison.php
@@ -2,22 +2,22 @@
 require ("../Modele/DonneeCapt.php");
 require("../Vue/Pages/Gestion_Maison.php");
 require ('../Modele/connexion_db.php');
-function import(){
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
-    curl_setopt($ch, CURLOPT_HEADER, FALSE);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    $data_tab = str_split($data,19);
-    $trame = $data_tab[1];
-    list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) =sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-    //$donn=$db->query('SELECT Datas FROM donnees WHERE idCapteur="'.$n.'"');
-    //$donnf=$donn->fetch;
-    $DATE=$year+$month+$day+$hour+$min+$sec;
-    $sql='INSERT "'.$a.'","'.$n.'","'.$DATE.'","'.$v.'" INTO donnees';
-    $db->exec($sql);
-    return ;
+$i=0;
+while($i=0){
+    function import(){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        $data_tab = str_split($data,19);
+        $trame = $data_tab[1];
+        list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) =sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+        $DATE=$year+$month+$day+$hour+$min+$sec;
+        $sql='INSERT "'.$a.'","'.$n.'","'.$DATE.'","'.$v.'" INTO donnees';
+        $db->exec($sql);
+    }
 }
 
 
--- Vue/Pages/Gestion_Maison.php
+++ Vue/Pages/Gestion_Maison.php
@@ -5,6 +5,33 @@
     $idMaison=1;
     $pieces=listsalles($db,$idMaison);
     $capteurs=listetypescapteurs($db,1);
+/*
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    $data_tab = str_split($data,19);
+    $i=0;
+    foreach($data_tab as $tab){
+        $i=$i+1;
+        list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) =sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+        $DATE=$year+$month+$day+$hour+$min+$sec;
+        $verif='SELECT idData FROM donnees WHERE idCapteur="'.$n.'" AND donnees.Date="'.$DATE.'"';
+        $dbverif=$db->query($verif);
+        if($dbverif == NULL) {
+            $trame = $data_tab[$i];
+            $sql = 'INSERT "' . $a . '","' . $n . '","' . $DATE . '","' . $v . '" INTO donnees';
+            $db->exec($sql);
+        }
+        else{echo(NULL);}
+    }
+*/
+
+
+
 
 ?>
 <!DOCTYPE html>
