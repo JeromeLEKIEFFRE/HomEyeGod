6441ff9ce549eac1736dbc420cbe240fdece3575
EmericFAUVEAU <emeric.fauveau@isep.fr>
2017-06-27 13:01:59 +0200

    Update

--- Modele/recup.php
+++ Modele/recup.php
@@ -4,25 +4,23 @@ include('connexion.php');
 
 // CONSULTATION ADMIN
 function recup_all_broken_capt($db){
-    $sql='SELECT utilisateurs.NomUtilisateur,typecapteur.TypeName,pieces.Nom
+    $sql='SELECT utilisateurs.NomUtilisateur,typecapteur.TypeName,pieces.Nom,utilisateurs.Mail
           FROM utilisateurs JOIN capteurs ON utilisateurs.idUtilisateur = capteurs.idUtilisateur
                             JOIN typecapteur ON capteurs.idCapteur = typecapteur.TypeValue
-                            JOIN pieces ON capteurs.idPièce = pieces.idPiece
+                            JOIN pieces ON capteurs.idPiece = pieces.idPiece
           WHERE capteurs.Etat = 3' ;
     $option="";
     $r = $db->query($sql);
-    $rf=$r->fetchAll();
-    foreach ($rf as $row)
-        $option.="<li>". $row["NomUtilisateur"].",capteur en panne de type ".$row["TypeName"].", dans la pièce ".$row["Nom_piece"].", contact:".$row['Mail']. "</li>";
+    foreach ($r as $row)
+        $option.="<li>". $row['NomUtilisateur'].",capteur en panne de type ".$row['TypeName'].", dans la pièce ".$row['Nom'].", contact:".$row['Mail']. "</li>";
     return $option;
 }
 function recup_name($db){
-    $return = $db -> query('SELECT NomUtilisateur,RoleName FROM utilisateurs JOIN roles ON utilisateurs.Roles = roles.RoleValue ORDER BY NomUtilisateur ASC ');
+    $return = $db -> query('SELECT utilisateurs.NomUtilisateur,roles.RoleName FROM utilisateurs JOIN roles ON utilisateurs.Roles = roles.RoleValue ORDER BY NomUtilisateur ASC ');
     $option = "<div class='nom_user'> <p>Nom d'utilisateur</p>" ;
     $option2 = "<div class='role_user'> <p>Rôle</p> " ;
-    $rf=$return->fetchAll();
-    foreach ($rf as $row) {
-        $option .= "<p id= ".$row['NomUtilisateur']." onclick='fill_div_info(".$row['NomUtilisateur'].")'>".$row['NomUtilisateur'] ."</p><br/>";
+    foreach ($return as $row) {
+        $option .= "<p id= ".$row['NomUtilisateur']." onclick='fill_div_info(this.id)'>".$row['NomUtilisateur'] ."</p><br/>";
         $option2 .= "<p>".$row['RoleName']."</p><br/>";
 
     }
@@ -77,15 +75,12 @@ function recup_text($db,$selected_text){
 }
 
 function recup_user_info($db,$id){
-    $r = $db->query('SELECT * FROM utilisateurs  JOIN maisons ON maisons.idUtilisateur = utilisateurs.idUtilisateur JOIN typevoie ON maisons.TypeVoie = typecapteur.TypeValue WHERE utilisateurs.NomUtilisateur ="'.$id.'"');
-    $option="<div class='user_content'> Informations";
-    $option2="<div class='house_content'> Maison(s)";
-    $rf=$r->fetchAll();
-    foreach ($rf as $row){
-        echo $row['NomUtilisateur'];
-        $option.= "Nom d'utilisateur : ".$row['NomUtilisateur']."</br>"."Nom : ".$row['Nom']."</br>"."Prénom : ".$row['Prenom']."</br>"."Adresse E-Mail : ".$row['Mail']."</br>"."Numéro de téléphone".$row['Numero']."</br>";
-        $option2.= "Adresse : ".$row['numero_voie']." ".$row['TypeName']." ".$row['Voie'].", ".$row['code_postal'].", ".$row['Ville'];
-    }
+    $r = $db->query('SELECT utilisateurs.NomUtilisateur,utilisateurs.Nom,utilisateurs.Prenom,utilisateurs.Mail,utilisateurs.Numero,maisons.numero_voie,typevoie.TypeName,maisons.Voie,maisons.code_postal,maisons.Ville FROM utilisateurs  JOIN maisons ON maisons.idUtilisateur = utilisateurs.idUtilisateur JOIN typevoie ON maisons.TypeVoie = typevoie.TypeValue WHERE utilisateurs.NomUtilisateur ="'.$id.'"');
+    $option="<div class='user_content'> Informations<br/>";
+    $option2="<div class='house_content'> Maison(s)<br/>";
+    $row=$r->fetch();
+    $option.= "Nom d'utilisateur : ".$row['NomUtilisateur']."<br/>"."Nom : ".$row['Nom']."</br>"."Prenom : ".$row['Prenom']."<br/>"."Adresse E-Mail : ".$row['Mail']."<br/>"."Numéro de téléphone".$row['Numero']."<br/>";
+    $option2.= "Adresse: ".$row['numero_voie']." ".$row['TypeName']." ".$row['Voie'].", ".$row['code_postal'].", ".$row['Ville'];
     $option.="</div>";
     $option2.="</div>";
     return array($option,$option2);
